a2bd90744318400e707b4cbe81f5f0c6e8d81442
st44100 <st44100@gmail.com>
2020-01-27 12:47:46 +0900

    feat(collide): yay collide!

--- collide.js
+++ collide.js
@@ -1,14 +1,5 @@
-
-let a;
-
-let Ball1Y;
-let BallSpeed = 10;
-
-const BallSize = {
-  w: 20,
-  h: 20
-};
-
+const CANVAS_W = 720;
+const CANVAS_H = 320;
 class Ball {
   constructor({
     x,
@@ -21,12 +12,15 @@ class Ball {
     this.y = y || 0;
     this.h = h || 10;
     this.w = w || 10;
-    this.speed = speed || 10;
+    this.speed = speed || 0;
   }
 
   update(dSpeed) {
     this.speed -= dSpeed;
-    this.y = this.y + dSpeed;
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+    this.y = this.y + this.speed;
 
     if (this.y > height) {
       this.y = 0;
@@ -40,37 +34,61 @@ class Ball {
       this.w
     );
   }
+
+  setSpeed(speed) {
+    this.speed = speed;
+  }
+
+  getPos() {
+    return {
+      x: this.x,
+      y: this.y,
+      w: this.w,
+      h: this.h
+    }
+  }
 }
 
 let Ball1 = new Ball({
-  x: 10,
-  y: 10
+  x: (CANVAS_W + 20)/2,
+  y: 20,
+  h: 20,
+  w: 20,
+  speed: 10
+});
+
+let Ball2 = new Ball({
+  x: (CANVAS_W + 20)/2,
+  y: 200,
+  h: 20,
+  w: 20,
+  speed: 0
 });
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(CANVAS_W, CANVAS_H);
   stroke(255);
-  a = height / 2;
-  Ball1Y = BallSize.h;
 }
 
 function draw() {
   background(51);
+
+  const posBall1 = Ball1.getPos();
+  const posBall2 = Ball2.getPos();
+
+  if (
+    posBall1.x >= posBall2.x &&
+    (posBall1.x + posBall1.w) <= (posBall2.x + posBall2.w) &&
+    posBall1.y + posBall1.h >= posBall2.y
+  ) {
+    Ball2.setSpeed(5);
+    Ball1.setSpeed(0);
+  } else {
+    Ball2.update(0);
+  }
+  Ball1.update(0.2);
+  Ball2.update(0.2);
+
   Ball1.draw();
-  Ball1.update(2);
-  ellipse(
-    (width - BallSize.w)/2,
-    Ball1Y,
-    BallSize.w
-  );
-  if (BallSpeed > 0) {
-    BallSpeed -= 0.15;
-  }
-  Ball1Y += BallSpeed;
-
-
-  if (Ball1Y > height - BallSize.h) {
-    Ball1Y = BallSize.h;
-    BallSpeed = 10;
-  }
+  Ball2.draw();
 }
